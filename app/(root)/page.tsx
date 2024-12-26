62a367b8c6d408707d22c63218e7ef3af077b53c
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2024-12-26 09:55:34 +0800

    Add StartupCard component and implement startup listing; enhance Home page with search results display and date formatting utility

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,26 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
-export default async function Home({ searchParams }: {
+export default async function Home({
+  searchParams,
+}: {
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _id: 1,
+      title: "We Robot",
+      description: "We Robot is a robot that can do anything.",
+      category: "Robotics",
+      image:
+        "https://images.unsplash.com/photo-1625314897518-bb4fe6e95229?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      author: { _id: 1, name: "Handa" },
+      views: 55,
+      _createdAt: new Date(),
+    },
+  ];
 
   return (
     <>
@@ -18,6 +35,22 @@ export default async function Home({ searchParams }: {
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts?.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
