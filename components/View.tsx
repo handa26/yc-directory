de480d4219d45452d982fe45eb5851243515dfac
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2024-12-29 20:30:56 +0800

    Implement view tracking by updating total views on the View component; add write client for Sanity and configure environment token

--- components/View.tsx
+++ components/View.tsx
@@ -1,12 +1,23 @@
+import { after } from "next/server";
+
 import Ping from "@/components/Ping";
 
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
 
   return (
     <div className="view-container">
@@ -15,7 +26,9 @@ const View = async ({ id }: { id: string }) => {
       </div>
 
       <p className="view-text">
-        <span className="font-black">{totalViews} {totalViews > 1 ? "Views" : "View"}</span>
+        <span className="font-black">
+          {totalViews} {totalViews > 1 ? "Views" : "View"}
+        </span>
       </p>
     </div>
   );
---
+++ sanity/lib/write-client.ts
@@ -0,0 +1,16 @@
+import "server-only"; // This file will only be included in server build
+import { createClient } from "next-sanity";
+
+import { apiVersion, dataset, projectId, token } from "../env";
+
+export const writeClient = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  token,
+});
+
+if (!writeClient.config().token) {
+  throw new Error("Write client is missing a token");
+}
