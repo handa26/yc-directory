32405b96c022382e9887ea1e24733c976ffb79d2
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2025-01-03 12:43:31 +0800

    Add Playlist schema and query; fetch editor picks in startup page

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -6,11 +6,11 @@ import markdownit from "markdown-it";
 
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 import { client } from "@/sanity/lib/client";
-import { STARTUP_QUERY_BY_ID } from "@/sanity/lib/queries";
+import { STARTUP_QUERY_BY_ID, PLAYLIST_BY_SLUG_QUERY } from "@/sanity/lib/queries";
 import { formatDate } from "@/lib/utils";
-import { StartupTypeCard } from "@/components/StartupCard";
 
 export const experimental_ppr = true;
 
@@ -19,7 +19,11 @@ const md = markdownit();
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const post: StartupTypeCard = await client.fetch(STARTUP_QUERY_BY_ID, { id });
+  // Fetch startup by id and editor picks playlist
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_QUERY_BY_ID, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: "editor-picks"}),
+  ]);
 
   if (!post) {
     return notFound();
@@ -80,7 +84,18 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
         <hr className="divider" />
 
-        {/* TODO: Editor selected startups */}
+        {editorPosts.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard, index: number) => (
+                <StartupCard key={index} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
+
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
           <View id={id} />
         </Suspense>
